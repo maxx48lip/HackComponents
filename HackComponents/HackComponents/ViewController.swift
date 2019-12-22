e05924f09a86c1a27aa132976f6f582e0ff14414
maxx48lip <44535856+maxx48lip@users.noreply.github.com>
2019-12-22 04:27:55 +0300

    Add files via upload

---
+++ HackComponents/HackComponents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackComponents
+//
+//  Created by Max Pavlov on 22/12/2019.
+//  Copyright © 2019 Pavlov Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
