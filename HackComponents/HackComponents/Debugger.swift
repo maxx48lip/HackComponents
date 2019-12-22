53d714a4bdddf0158c671fa49e622a023176e18d
Павлов Максим Юрьевич <Pavlov.M.Y@sberbank.ru>
2019-12-22 16:11:54 +0300

    Добавил деббагер/логгер

---
+++ HackComponents/HackComponents/Debugger.swift
@@ -0,0 +1,35 @@
+//
+//  Debugger.swift
+//  HackComponents
+//
+//  Created by Max Pavlov on 22/12/2019.
+//  Copyright © 2019 Pavlov Max. All rights reserved.
+//
+
+import Foundation
+
+/// Дебаггер
+enum Debugger {
+
+	/// Тип логгирования
+	enum LogType: String {
+		case info = "ℹ️ℹ️ℹ️ [Info] - "
+		case error = "⛔️❌🆘 [Error] - "
+		case magic = "🌈🎉🧚‍♀️ [Magic] - "
+	}
+
+	/// Функция для логгирования
+	///
+	/// - Parameters:
+	///   - type: тип логгирования
+	///   - logString: строка логгирования
+	static func log(type: LogType, logString: String?) {
+		let prefix = type.rawValue
+		let mainLogString = logString ?? "Trolling expected" + "\n"
+		let timestamp = DateFormatter.localizedString(from: NSDate() as Date, dateStyle: .short, timeStyle: .medium)
+		let suffix = "🕑      "
+
+		let result = prefix + mainLogString + suffix + timestamp
+		print(result)
+	}
+}
--- HackComponents/HackComponents/ViewController.swift
+++ HackComponents/HackComponents/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		Debugger.log(type: .error, logString: nil)
 		// Do any additional setup after loading the view.
 	}
 
